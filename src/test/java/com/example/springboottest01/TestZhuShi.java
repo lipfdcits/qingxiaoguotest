7dad2057c10872992b0604c21fbb05f3e1120c1a
lipfdcits <lipf_dcits@163.com>
2020-05-15 13:14:37 +0800

    testJenkins

---
+++ src/test/java/com/example/springboottest01/TestZhuShi.java
@@ -0,0 +1,30 @@
+package com.example.springboottest01;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestZhuShi {
+    @BeforeTest
+    public void test1(){
+        System.out.println("这是@BeforeTest");
+    }
+    @AfterTest
+    public void test2(){
+        System.out.println("这是@AfterTest");
+    }
+    @Test
+    public void test3(){
+        System.out.println("这是@Test1");
+    }
+    @Test
+    public void test4(){
+        System.out.println("这是@Test2");
+    }
+    @Test
+    public void test5(){
+        System.out.println("这是@Test3");
+    }
+
+
+}
